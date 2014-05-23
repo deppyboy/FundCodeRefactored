fcda367e36bc976a26d36d16406c0b6fc19d6d49
Timothy Dees <timothy.dees@gmail.com>
2014-05-23 09:58:50 -0700

    Cleaned up the types
    Removed Normal/Uniform from the data type and into the code.

--- rng.hs
+++ rng.hs
@@ -5,28 +5,31 @@ import Data.Word
 
 data Distribution = Normal | Uniform deriving (Show, Eq)
 
-data PrefetchRands = PrefetchRands { fetched :: [Double], gen :: PureMT, randType :: Distribution } deriving (Show)
+data PrefetchRands = PrefetchRands { fetched :: [Double], gen :: PureMT } deriving (Show)
 
 x = 64 :: Word64
 
 j = pureMT x
 k = PrefetchRands [] j
 
+boxMuller :: (Floating a)=>a->a->(a,a)
 boxMuller r1 r2 = (f cos, f sin)
 	where f a = sqrt (-2.0*log r1) * (a $ 2.0*pi*r2)
 
-fetchrand (PrefetchRands xs rng Normal) | xs==[] = (bm1, PrefetchRands [bm2] gen2)
+fetchrand :: PrefetchRands->Distribution->(Double, PrefetchRands)
+fetchrand (PrefetchRands xs rng) Normal | xs==[] = (bm1, PrefetchRands [bm2] gen2)
                                         | otherwise = (head xs, PrefetchRands (tail xs) rng)
                                              where
 						(rand1, gen1) = randomDouble rng
 						(rand2, gen2) = randomDouble gen1
 						(bm1, bm2) = boxMuller rand1 rand2
 
-fetchrand (PrefetchRands _ rng Uniform) = (rand, PrefetchRands [] newrng)
+fetchrand (PrefetchRands _ rng) Uniform = (rand, PrefetchRands [] newrng)
 			where (rand, newrng) = randomDouble rng
 
-fetchrands (PrefetchRands myfetch mygen mydist) = fetchaccum [] (PrefetchRands myfetch mygen)
+fetchrands :: (Eq a, Num a)=>PrefetchRands->Distribution->a->([Double], PrefetchRands)
+fetchrands (PrefetchRands myfetch mygen) mydist = fetchaccum [] (PrefetchRands myfetch mygen)
 		where
 			fetchaccum accum pf 0 = (accum, pf)
 			fetchaccum accum pf x = fetchaccum (newnorm:accum) newpfr (x-1)
-				where (newnorm, newpfr) = fetchrand $ pf mydist
+				where (newnorm, newpfr) = fetchrand pf mydist
