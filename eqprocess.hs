ba1f7ccb9c50bb0655cfaea49b4859a33f711eb7
Timothy Dees <timothy.dees@gmail.com>
2014-05-23 12:52:43 -0700

    Refactored to use state monad
    Refactored code to use state monad.

--- eqprocess.hs
+++ eqprocess.hs
@@ -4,8 +4,10 @@ import Data.Complex
 import YC
 import VolSurf
 import RNG
+import Control.Monad.State
+import qualified Data.Vector.Unboxed as U
 
-data Lognormal a = Lognormal { volatility :: a } deriving (Show, Eq)
+data Lognormal a = Lognormal { lnvolatility :: a } deriving (Show, Eq)
 
 data Heston a = Heston { v0 :: a,
 			 vf :: a, 
@@ -13,12 +15,14 @@ data Heston a = Heston { v0 :: a,
 			 volvol :: a, 
 			 rho :: a } deriving (Show, Eq)
 
-data Dupire a = Dupire { rf :: YieldCurve a,
-			 div :: YieldCurve a,
-			 s0  :: a,
-			 vols :: VolSurf a}
+data Dupire a = Dupire { vols :: VolSurf a,
+			 s0   :: a} deriving (Show, Eq)
 
-data MCState a = MCState { model :: a Double, esgstate :: PrefetchRands }
+data MCState a = MCState { model :: a Double, 
+			   rngstate :: PrefetchRands, 
+			   level :: Double,
+			   rfproc :: YieldCurve Double,
+			   divproc :: YieldCurve Double }
 
 class CharFunc a where
 	charfuncfactory :: (RealFloat b)=>a b->b->(Complex b->Complex b)
@@ -53,64 +57,88 @@ instance CharFunc Heston where
 			volvolc = convert volvol
 
 class Discretize a where
-	evolve ::  MCState a->         --model + rng
-		   YieldCurve Double-> --rf curve
-		   YieldCurve Double-> --carry curve
-		   Double->            --start level
-		   Double->            --start time
+	evolve ::  Double->            --start time
 		   Double->            --end time
-		   (Double, MCState a) --final value and state
+		   State (MCState a) Double --final value and state
 
-	genpath :: MCState a->         --model + rng
-		   YieldCurve Double-> --risk-free rate
-		   YieldCurve Double-> --carry curve
-		   Double->            --start level
-		   Double->            --start time
+	genpath :: Double->            --start time
 		   Double->            --end time
 		   Int->               --steps
-		   ([Double], MCState a)  --final model + rng
-	genpath initstate rf div lvl t1 t2 intervals = genpath' [lvl] initstate t1 intervals
-		where
-			genpath' lvls mcs start 1 = (lvls++[newlvl], mcs')
-				where
-					(newlvl, mcs') = evolve mcs rf div (last lvls) start t2
-			genpath' lvls mcs start n = genpath' (lvls++[newlvl]) mcs' (start+dt) (n-1)
-				where
-					(newlvl, mcs') = evolve mcs rf div (last lvls) start (start+dt)
-					dt = (t2-start)/(fromIntegral n)
+		   State (MCState a) [Double] --final model + rng
+	genpath t1 t2 intervals = do
+		let
+			genpath' lvls start 1 = do
+				newlevel <- evolve start t2
+				return (lvls++[newlevel])
+			genpath' lvls start n = do
+				let dt = (t2-start)/(fromIntegral n)
+				newlvl <- evolve start (start+dt)
+				solution <- genpath' (lvls++[newlvl]) (start+dt) (n-1)
+				return solution
+		result<-genpath' [] t1 intervals
+		return result
+				
 
+randWrapper :: Distribution->State (MCState a) Double
+randWrapper x = state $ f
+	where 
+		f (MCState a1 myrng a3 a4 a5) = (randval, MCState a1 newrng a3 a4 a5)
+			where (randval, newrng) = fetchrand myrng x
 
 instance Discretize Lognormal where
-	evolve (MCState (Lognormal vol) rng) rf div lvl t1 t2 = (newlvl, MCState (Lognormal vol) newrng)
-		where
-			(rand, newrng) = fetchrand rng Normal
+	evolve t1 t2 = do
+		rand<-randWrapper Normal
+		internal<-get
+		let 
+			(model, rng, lvl, rf, div) = unwrap internal
 			t = t2-t1
 			r = forward rf t1 t2 - forward div t1 t2
+			vol = lnvolatility model
 			rate = (*) t (r-vol*vol*t/2.0)
 			newlvl = lvl*exp (rate+rand*vol*(sqrt t))
+		put $ MCState model rng newlvl rf div
+		return $ newlvl
 
+teststate = MCState (Lognormal 0.2) k 100.0 (FlatCurve 0.05) (FlatCurve 0.02)
 instance Discretize Dupire where
-	evolve (MCState dp rng) rf div lvl t1 t2 = (newlvl, MCState dp newrng)
-		where
-			lvol = localvol dp lvl t1
-			(newlvl, MCState _ newrng) = evolve (MCState (Lognormal lvol) rng) rf div lvl t1 t2
+	evolve t1 t2 = do
+		rand<-randWrapper Normal
+		internal<-get
+		let 
+			(model, rng, lvl, rf, div) = unwrap internal
+			t = t2-t1
+			r = forward rf t1 t2 - forward div t1 t2
+			lvol = localvol model rf div lvl t1
+			rate = (*) t (r-lvol*lvol*t/2.0)
+			newlvl = lvl*exp (rate+rand*lvol*(sqrt t))
+		put $ MCState model rng newlvl rf div
+		return $ newlvl
 
 instance Discretize Heston where
-	evolve (MCState (Heston vinit vfinal kap sigma correl) rng) rf div lvl t1 t2 = (newlvl, MCState (Heston flipv vfinal kap sigma correl) newrng)
-		where
-			([x,y], newrng) = fetchrands rng Normal 2
-			z = correl*x+sqrt (1-correl*correl)*y
+	evolve t1 t2 = do
+		x<-randWrapper Normal
+		y<-randWrapper Normal
+		internal<-get
+		let 
+			(model, rng, lvl, rf, div) = unwrap internal
+			(vinit, vfinal, kap, sigma, correl) = (\(Heston a b c d e)->(a,b,c,d,e)) model
+			z = x*correl+sqrt(1-correl*correl)*y
 			r = forward rf t1 t2 - forward div t1 t2
 			t = t2-t1
 			newlvl = lvl * exp (r-vinit/2.0+x*sqrt (vinit*t))
 			newv = (sqrt vinit+sigma/2.0*(sqrt t)*z)^2-kap*(vinit-vfinal)*t-sigma*sigma*t/4.0
 			flipv = if newv>0.0 then newv else -newv
+		put (MCState (Heston flipv vfinal kap sigma correl) rng newlvl rf div)
+		return newlvl
 
+unwrap :: MCState t-> (t Double, PrefetchRands, Double, YieldCurve Double, YieldCurve Double)
+unwrap (MCState a b c d e) = (a,b,c,d,e)
 
-localvol (Dupire riskf div s vs) k t | w==0.0 || solution<0.0 = sqrt dwdt
-				     | otherwise = sqrt solution
+localvol :: (RealFloat a, U.Unbox a)=>Dupire a->YieldCurve a->YieldCurve a->a->a->a
+localvol (Dupire vs s) rcurve dcurve k t | w==0.0 || solution<0.0 = sqrt dwdt
+			                 | otherwise = sqrt solution
 	where
-		fwd t1 t2 = exp $ (forward riskf t1 t2) - (forward div t1 t2)
+		fwd t1 t2 = exp $ (forward rcurve t1 t2) - (forward dcurve t1 t2)
 		f = s*(fwd 0.0 t)
 		y = log $ k/f
 		dy = 1.0E-6
@@ -127,4 +155,3 @@ localvol (Dupire riskf div s vs) k t | w==0.0 || solution<0.0 = sqrt dwdt
 					strikept = k*fwd t (t+dt)
 					strikemt = k/(fwd t (t-dt))
 		solution = dwdt/(1.0-y/w*dwdy+0.25*(-0.25-1.0/w+y*y/w/w)*dwdy*dwdy+0.5*d2wdy2)
-
