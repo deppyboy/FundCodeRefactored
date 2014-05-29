aed8b556ee88b8bc7234bf3830b29a5dc604b9e7
Timothy Dees <timothy.dees@gmail.com>
2014-05-29 10:39:34 -0700

    Re-factored volsurf with splines as internals
    Should speed things up a bit for multiple interpolations.

--- volsurf.hs
+++ volsurf.hs
@@ -1,17 +1,25 @@
 module VolSurf where
 
-import HassetsMath
+import CubicSpline
 import qualified Data.Vector.Unboxed as U
 
---TODO: Redo with splines as internals!
-
-data VolSurf a =   VolSurf { 	grid :: [[a]],
+--rows of grid are maturities, columns are strikes
+data VolSurf a =   VolSurf { 	grid :: [[a]], 
 				maturities :: [a],
-				strikes :: [a] } deriving (Show, Eq)
+				strikes :: [a],
+				splines ::  [CSpline a]} deriving (Show, Eq)
+
+mkVolSurf :: (Floating a, Ord a, U.Unbox a)=>[[a]]->[a]->[a]->VolSurf a
+mkVolSurf ingrid inmats instrikes = VolSurf ingrid inmats instrikes csplines
+	where
+		csplines = [createSpline instrikes k | k<-ingrid]
 
 volatility :: (Floating a, Ord a, U.Unbox a)=>VolSurf a->a->a->a
-volatility (VolSurf grid maturities strikes) strike mat = interpolate2dcubic grid strikes maturities strike mat
+volatility (VolSurf _ mats _ csplines) strike mat = evalSpline newspline mat
+	where newspline = createSpline mats [evalSpline k strike | k<-csplines]
+
 
 variance :: (Floating a, Ord a, U.Unbox a)=>VolSurf a->a->a->a
 variance vs strike mat = vol*vol*mat
 	where vol = volatility vs strike mat
+
