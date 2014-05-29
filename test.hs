1c62f0703009dd480e38c36fd29a72db59e048f9
Timothy Dees <timothy.dees@gmail.com>
2014-05-29 11:41:45 -0700

    Added LV test code
    Stash for a future date.

--- test.hs
+++ test.hs
@@ -17,3 +17,12 @@ ln = Lognormal 0.2 "SPX"
 h = Heston 0.04 0.04 1.0 0.2 (-0.7) "SPX"
 x = Equity "SPX" 100.0 myyc mydiv vs h
 v = vanillaopt (VanillaOption 100.0 1.0 1.0 Put) x
+
+lv = do
+	let dt2013 = fromGregorian 2013 12 31
+	myyc <- loadYC dt2013
+	mydiv <- getDivFromDB "MSCI EAFE" dt2013
+	myvol <- getVols "MSCI EAFE" dt2013
+	let lv = \(x,y)->localvol (Dupire myvol 1915.6) myyc mydiv x y
+	return $ lv (1500.0, 3.2)
+
