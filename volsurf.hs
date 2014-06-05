a48a7eb5ac5022573d89dcb18fea84d757704b8f
Timothy Dees <timothy.dees@gmail.com>
2014-06-04 17:37:18 -0700

    Boxed vectors

--- volsurf.hs
+++ volsurf.hs
@@ -1,7 +1,6 @@
 module VolSurf where
 
 import CubicSpline
-import qualified Data.Vector.Unboxed as U
 
 --rows of grid are maturities, columns are strikes
 data VolSurf a =   VolSurf { 	grid :: [[a]], 
@@ -9,17 +8,17 @@ data VolSurf a =   VolSurf { 	grid :: [[a]],
 				strikes :: [a],
 				splines ::  [CSpline a]} deriving (Show, Eq)
 
-mkVolSurf :: (Floating a, Ord a, U.Unbox a)=>[[a]]->[a]->[a]->VolSurf a
+mkVolSurf :: (Floating a, Ord a)=>[[a]]->[a]->[a]->VolSurf a
 mkVolSurf ingrid inmats instrikes = VolSurf ingrid inmats instrikes csplines
 	where
 		csplines = [createSpline instrikes k | k<-ingrid]
 
-volatility :: (Floating a, Ord a, U.Unbox a)=>VolSurf a->a->a->a
+volatility :: (Floating a, Ord a)=>VolSurf a->a->a->a
 volatility (VolSurf _ mats _ csplines) strike mat = evalSpline newspline mat
 	where newspline = createSpline mats [evalSpline k strike | k<-csplines]
 
 
-variance :: (Floating a, Ord a, U.Unbox a)=>VolSurf a->a->a->a
+variance :: (Floating a, Ord a)=>VolSurf a->a->a->a
 variance vs strike mat = vol*vol*mat
 	where vol = volatility vs strike mat
 
--- yc.hs
+++ yc.hs
@@ -2,7 +2,6 @@ module YC where
 
 import HassetsMath
 import CubicSpline
-import qualified Data.Vector.Unboxed as U
 
 data YieldCurve a = LinearPar	 {  lpmats  :: [a], 
                  	            linearrates :: [a] }   |
@@ -27,7 +26,7 @@ stripcurvelinearpar mats rates = LinearPar times $ recursivestrip times interpra
 			where disc = (1.0-x/2.0*cumdisc)/(1.0+x/2.0)
 
 --takes a par curve and uses a cubic spline on pars and linear on the log-discs
-stripcurvecubicparlogdisc :: (RealFloat a, Enum a, Ord a, U.Unbox a)=>[a]->[a]->YieldCurve a
+stripcurvecubicparlogdisc :: (RealFloat a, Enum a, Ord a)=>[a]->[a]->YieldCurve a
 stripcurvecubicparlogdisc mats rates = CubicParLinearLogDisc times $ recursivestrip times interprates 0.0
 	where
 		times = [0.5,1.0..40]
@@ -37,7 +36,7 @@ stripcurvecubicparlogdisc mats rates = CubicParLinearLogDisc times $ recursivest
 			where disc = (1.0-x/2.0*cumdisc)/(1.0+x/2.0)
 
 --takes a par curve and uses a cubic spline on pars cubic spline on rates
-stripcurvecubiccubic :: (RealFloat a, Enum a, Ord a, U.Unbox a)=>[a]->[a]->YieldCurve a
+stripcurvecubiccubic :: (RealFloat a, Enum a, Ord a)=>[a]->[a]->YieldCurve a
 stripcurvecubiccubic mats rates = CubicParCubicSpot $ createSpline times $ recursivestrip times interprates 0.0
 	where
 		times = [0.5,1.0..40]
@@ -48,21 +47,21 @@ stripcurvecubiccubic mats rates = CubicParCubicSpot $ createSpline times $ recur
 
 
 --calculates discount factors
-disc :: (RealFloat a, Ord a, U.Unbox a)=>YieldCurve a->a->a
+disc :: (RealFloat a, Ord a)=>YieldCurve a->a->a
 disc (LinearPar mats rates) t = exp $ (*) (-t) $ interpolate1d mats rates t
 disc (CubicParLinearLogDisc mats logdiscs) t = exp $ interpolate1d mats logdiscs t
 disc (CubicParCubicSpot cs) t = exp $ (*) (-t) $ evalSpline cs t
 disc (FlatCurve r) t = exp $ -r*t
 
 --calculates forward rates
-forward :: (RealFloat a, Ord a, U.Unbox a)=>YieldCurve a->a->a->a
+forward :: (RealFloat a, Ord a)=>YieldCurve a->a->a->a
 forward yc t1 t2 = -log(d2 / d1)/(t2-t1)
 	where
 		d1 = disc yc t1
 		d2 = disc yc t2
 
 --calculates par rates
-parswap :: (RealFloat a, Enum a, Ord a, U.Unbox a)=>YieldCurve a->a->a
+parswap :: (RealFloat a, Enum a, Ord a)=>YieldCurve a->a->a
 parswap yc tmat = (1.0-last discs) / sum discs *2.0
 	where discs = map (disc yc) [0.5,1.0..tmat]
 
