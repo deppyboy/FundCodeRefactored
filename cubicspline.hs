c3e7994b500faff97818b2467f1113cfa915d8e8
Timothy Dees <timothy.dees@gmail.com>
2014-06-04 17:35:46 -0700

    Moving to boxed vectors

--- cubicspline.hs
+++ cubicspline.hs
@@ -1,38 +1,36 @@
 module CubicSpline where
 
-import qualified Data.Vector.Unboxed as U
-
-
+import qualified Data.Vector as V
 
 -- | The 'CSpline' object holds three lists: one for the knots, one for the control points, and one for the moments.
-data CSpline a = CSpline { knots   :: U.Vector a,
-		 	   cps     :: U.Vector a,
-		 	   moments :: U.Vector a } deriving (Show, Eq)
+data CSpline a = CSpline { knots   :: V.Vector a,
+		 	   cps     :: V.Vector a,
+		 	   moments :: V.Vector a } deriving (Show, Eq)
 
 -- | The 'createSpline' function creates a spline from a list of knots and control points.
-createSpline :: (U.Unbox a, Fractional a, Ord a)=>[a]->[a]->CSpline a
-createSpline knotvals controls = CSpline knotvals' controls' $ tdmaSolver mu (U.replicate (U.length d) 2.0) lambda d
+createSpline :: (Fractional a, Ord a)=>[a]->[a]->CSpline a
+createSpline knotvals controls = CSpline knotvals' controls' $ tdmaSolver mu (V.replicate (V.length d) 2.0) lambda d
 	where
-		knotvals' = U.fromList knotvals
-		controls' = U.fromList controls
-		h = U.zipWith (-) (U.tail knotvals') (U.init knotvals')
-		lambda = U.cons 0.0 $ U.zipWith (\x y->x/(x+y)) (U.tail h) (U.init h)
-		mu = U.snoc (U.map (\x->1.0-x) lambda) 0.0
+		knotvals' = V.fromList knotvals
+		controls' = V.fromList controls
+		h = V.zipWith (-) (V.tail knotvals') (V.init knotvals')
+		lambda = V.cons 0.0 $ V.zipWith (\x y->x/(x+y)) (V.tail h) (V.init h)
+		mu = V.snoc (V.map (\x->1.0-x) lambda) 0.0
 		dj hj hj1 yj1 yj yjm1= 6.0/(hj+hj1)*((yj1-yj)/hj1-(yj-yjm1)/hj)
-		d = U.cons 0.0 $ U.zipWith5 dj (U.init h) (U.tail h) 
-				(U.tail $ U.tail controls') (U.tail $ U.init controls') 
-				(U.init $ U.init controls') U.++ U.singleton 0.0
+		d = V.cons 0.0 $ V.zipWith5 dj (V.init h) (V.tail h) 
+				(V.tail $ V.tail controls') (V.tail $ V.init controls') 
+				(V.init $ V.init controls') V.++ V.singleton 0.0
 
 -- | Evaluates a CSpline object.
-evalSpline :: (U.Unbox a, Fractional a, Ord a)=>CSpline a->a->a
-evalSpline (CSpline ks ys ms) x | x<=U.head ks = U.head ys
-				| x>=U.last ks = U.last ys
-				| otherwise = splineCalc k1 k2 y1 y2 m1 m2
+evalSpline :: (Fractional a, Ord a)=>CSpline a->a->a
+evalSpline (CSpline ks ys ms) x | x<=V.head ks = V.head ys
+			        | x>=V.last ks = V.last ys
+			        | otherwise = splineCalc k1 k2 y1 y2 m1 m2
 					where
-						(kstart, kend) = U.partition (<x) ks
-						(k1, k2) = (U.last kstart, U.head kend)
-						getslice = U.slice (U.length kstart-1) 2
-						getvals y = (y U.! 0, y U.! 1)
+						(kstart, kend) = V.partition (<x) ks
+						(k1, k2) = (V.last kstart, V.head kend)
+						getslice = V.slice (V.length kstart-1) 2
+						getvals y = (y V.! 0, y V.! 1)
 						(y1, y2) = getvals $ getslice ys
 						(m1, m2) = getvals $ getslice ms
 						splineCalc k1' k2' y1' y2' m1' m2' = y1'+beta*term+gamma*term^2+
@@ -46,12 +44,12 @@ evalSpline (CSpline ks ys ms) x | x<=U.head ks = U.head ys
 			
 
 -- | A simple function to solve tridiagonal matricies.
-tdmaSolver :: (U.Unbox a, Fractional a, Ord a)=>U.Vector a->U.Vector a->U.Vector a->U.Vector a->U.Vector a
-tdmaSolver a b c d = U.reverse $ solution $ U.zip (U.cons 0.0 $ U.reverse cvals) (U.reverse dvals)
+tdmaSolver :: (Fractional a, Ord a)=>V.Vector a->V.Vector a->V.Vector a->V.Vector a->V.Vector a
+tdmaSolver a b c d = V.reverse $ solution $ V.zip (V.cons 0.0 $ V.reverse cvals) (V.reverse dvals)
 	where
 		cvals = scanlt0 (\priorc (a1,b1,c1)->c1/(b1-priorc*a1)) $ 
-					U.zip3 (U.cons 0.0 $ U.init a) (U.init b) c
+					V.zip3 (V.cons 0.0 $ V.init a) (V.init b) c
 		dvals = scanlt0 (\priord (a1,b1,c1,d1)->(d1-a1*priord)/(b1-a1*c1)) $ 
-					U.zip4 (U.cons 0.0 a) b (U.cons 0.0 cvals) d
+					V.zip4 (V.cons 0.0 a) b (V.cons 0.0 cvals) d
 		solution = scanlt0 (\priorx (cn, dn)->dn-cn*priorx)
-		scanlt0 f = U.tail . U.scanl f 0.0
+		scanlt0 f = V.tail . V.scanl f 0.0
