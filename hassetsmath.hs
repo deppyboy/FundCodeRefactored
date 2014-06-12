f1819e45e1e8183c510b5f4f227cfbf8841d8706
Timothy Dees <timothy.dees@gmail.com>
2014-06-12 11:28:57 -0700

    Cleanup and linting

--- hassetsmath.hs
+++ hassetsmath.hs
@@ -3,6 +3,7 @@ module HassetsMath where
 import CubicSpline
 import qualified Data.Vector as V
 
+
 interpolate1d :: (Fractional a, Ord a) => [a]->[a]->a->a
 interpolate1d (x1:x2:xs) (y1:y2:ys) xval | xval<=x1 = y1
 					 | xval>=x2 = interpolate1d (x2:xs) (y2:ys) xval
@@ -10,7 +11,7 @@ interpolate1d (x1:x2:xs) (y1:y2:ys) xval | xval<=x1 = y1
 interpolate1d _ (y1:_) _ = y1
 
 interpolate1dcubic :: (Fractional a, Ord a) => [a]->[a]->a->a
-interpolate1dcubic xs ys z = evalSpline (createSpline xs ys) z
+interpolate1dcubic xs ys = evalSpline (createSpline xs ys)
 
 interpolate2d :: (Fractional a, Ord a) => [[a]]->[a]->[a]->a->a->a
 interpolate2d grid xs ys xval yval = interpolate1d xs [interpolate1d ys row yval | row<-grid] xval
@@ -22,7 +23,7 @@ simpint :: (Enum a, Floating a)=>(a->a)->a->a->Int->a
 simpint f a b intervals = 
 	dx * ((V.head fy + V.last fy)/6.0 + V.sum(V.tail $ V.init fy)/3.0 + V.sum(V.tail fx)*2.0/3.0)
 		where
-                  dx = (b-a)/(fromIntegral intervals)
+                  dx = (b-a)/fromIntegral intervals
                   fy = V.map f $ V.enumFromStepN a dx (intervals+1)
                   fx = V.map f $ V.enumFromStepN (a-0.5*dx) dx (intervals+1)
 
