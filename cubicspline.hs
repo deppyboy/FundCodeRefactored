b1a2aedbfa5d159120dea58b1a69ef58447d976a
Timothy Dees <timothy.dees@gmail.com>
2014-06-12 11:29:30 -0700

    Use new vectorops library
    Refactored code to use new library

--- cubicspline.hs
+++ cubicspline.hs
@@ -1,6 +1,7 @@
 module CubicSpline where
 
 import qualified Data.Vector as V
+import VectorOps
 
 -- | The 'CSpline' object holds three lists: one for the knots, one for the control points, and one for the moments.
 data CSpline a = CSpline { knots   :: V.Vector a,
@@ -13,13 +14,15 @@ createSpline knotvals controls = CSpline knotvals' controls' $ tdmaSolver mu (V.
 	where
 		knotvals' = V.fromList knotvals
 		controls' = V.fromList controls
-		h = V.zipWith (-) (V.tail knotvals') (V.init knotvals')
-		lambda = begZero $ V.zipWith (\x y->x/(x+y)) (V.tail h) (V.init h)
-		mu = endZero $ V.map (\x->1.0-x) lambda
-		dj hj hj1 yj1 yj yjm1= 6.0/(hj+hj1)*((yj1-yj)/hj1-(yj-yjm1)/hj)
-		d = endZero $ begZero $ V.zipWith5 dj (V.init h) (V.tail h) 
-				(V.tail $ V.tail controls') (V.tail $ V.init controls') 
-				(V.init $ V.init controls')
+		h = V.tail knotvals' <-> V.init knotvals'
+		lambda = begZero $ V.tail h </> (V.tail h <+>  V.init h)
+		mu = endZero $ 1.0 <!-> lambda
+		(hj, hj1) = (V.init h, V.tail h)
+		(yj1, yj, yjm1) = (V.tail $ V.tail controls', 
+						   V.tail $ V.init controls', 
+						   V.init $ V.init controls')
+		d = endZero $ begZero $ 
+				6.0 <!/> (hj<+>hj1) <*> ((yj1<->yj)</>hj1<->(yj<->yjm1)</>hj)
 
 -- | Evaluates a CSpline object.
 evalSpline :: (Fractional a, Ord a)=>CSpline a->a->a
@@ -33,8 +36,8 @@ evalSpline (CSpline ks ys ms) x | x<=V.head ks = V.head ys
 										getvals y = (y V.! 0, y V.! 1)
 										(y1, y2) = getvals $ getslice ys
 										(m1, m2) = getvals $ getslice ms
-										splineCalc k1' k2' y1' y2' m1' m2' = y1'+beta*term+gamma*term^2+
-											delta*term^3
+										splineCalc k1' k2' y1' y2' m1' m2' = y1'+beta*term+gamma*term*term+
+											delta*term*term*term
 											where
 												gamma = m1'/2.0
 												beta = (y2'-y1')/h-(2*m1'+m2')*h/6.0
@@ -58,4 +61,4 @@ begZero :: (Fractional a, Ord a)=>V.Vector a->V.Vector a
 begZero = V.cons 0.0
 
 endZero :: (Fractional a, Ord a)=>V.Vector a->V.Vector a
-endZero = \x->V.snoc x 0.0
+endZero = (`V.snoc` 0.0)
